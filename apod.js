238fdc5935fbcf80a8a50b77a55a0c4288cd5150
Rayjan Wilson <rayjan.wilson@gmail.com>
2014-01-09 08:48:17 -0900

    updates

--- apod.js
+++ apod.js
@@ -5,42 +5,43 @@ var request = require('request');
 var fs = require('fs');
 
 var today = new Date();
-today = today.toJSON().substring(0,10);
+today = today.toJSON().substring(0, 10);
 
 var downloadDir = '/Users/rmwilson/Pictures/apod/';
 var downloadFile = today + '.jpg';
 
-fs.readdir(downloadDir, function(err, files){
-  if(! (downloadFile in files)){
-    fetchImage();
-  }
+fs.readdir(downloadDir, function(err, files) {
+    if (!(downloadFile in files)) {
+        fetchImage();
+    }
 
 });
 
-var fetchImage = function(){
-  var apodUri = 'http://apod.nasa.gov/apod/';
-  request({ uri: apodUri}, function (error, response, body) {
-    if (error && response.statusCode !== 200) {
-      console.log('Error when contacting apod.nasa.gov/apod/');
-    }
+var fetchImage = function() {
+    var apodUri = 'http://apod.nasa.gov/apod/';
+    request({
+        uri: apodUri
+    }, function(error, response, body) {
+        if (error && response.statusCode !== 200) {
+            console.log('Error when contacting apod.nasa.gov/apod/');
+        }
 
-    jsdom.env({
-      html: body,
-      scripts: [
-        'http://code.jquery.com/jquery.js'
-      ],
-      done: function (err, window) {
-        var $ = window.$;
+        jsdom.env({
+            html: body,
+            scripts: [
+                'http://code.jquery.com/jquery.js'
+            ],
+            done: function(err, window) {
+                var $ = window.$;
 
-        // jQuery is now loaded on the jsdom window created from 'agent.body'
-        var image = $('html body center p a img').attr("src");
-        var imageUri = apodUri + image;
-        var downloadUri = downloadDir+downloadFile;
-        request(imageUri).pipe(fs.createWriteStream(downloadUri));
-      }
+                // jQuery is now loaded on the jsdom window created from 'agent.body'
+                var image = $('html body center p a img').attr("src");
+                var imageUri = apodUri + image;
+                var downloadUri = downloadDir + downloadFile;
+                request(imageUri).pipe(fs.createWriteStream(downloadUri));
+            }
 
+        });
     });
-  });
 
-}
-
+};
